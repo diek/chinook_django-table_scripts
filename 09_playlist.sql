2706c1388a4cddf813a8016677f6e6a97cb1ed64
derrick kearney <diek@bellaliant.net>
2017-08-07 16:27:02 -0300

    10 scripts to populate Postgresql DB for Chinook
    - 01_artist.sql
    - 02_employee.sql
    - 03_album.sql
    - 04_genre_mediatype.sql
    - 05_track.sql
    - 06_customer.sql
    - 07_invoice.sql
    - 08_invoiceline.sql
    - 09_playlist.sql
    - 10_playlist_track.sql

---
+++ 09_playlist.sql
@@ -0,0 +1,19 @@
+INSERT INTO chinook.public.customer_playlist(id, play_list)
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (1, N'Music');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (2, N'Movies');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (3, N'TV Shows');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (4, N'Audiobooks');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (5, N'90’s Music');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (6, N'Audiobooks');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (7, N'Movies');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (8, N'Music');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (9, N'Music Videos');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (10, N'TV Shows');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (11, N'Brazilian Music');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (12, N'Classical');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (13, N'Classical 101 - Deep Cuts');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (14, N'Classical 101 - Next Steps');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (15, N'Classical 101 - The Basics');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (16, N'Grunge');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (17, N'Heavy Metal Classic');
+INSERT INTO chinook.public.customer_playlist(id, play_list) VALUES (18, N'On-The-Go 1');
---
+++ README.rst
@@ -0,0 +1,20 @@
+============================
+The chinook database and Django: Table Data
+============================
+
+Coded using:
+-----------------------
+- Django 1.11.4
+- Sublime Text 3
+- PostgreSQL 9.5
+
+
+Data Source
+----------------
+- Data from Luis Rocha's :
+`Chinook Database <https://github.com/lerocha/chinook-database>`_
+- Modified for Django
+
+Data Dump
+----------------
+The data dump was messy, once cleaned up I will add it
